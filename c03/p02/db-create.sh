f22e1c6b36de5af49df053fcb6dd1881e8d53c90
JuanJo Ciarlante <juanjosec@gmail.com>
2016-05-09 20:33:02 -0300

    [jjo] fixes from running it live (c03/p02)

--- c03/p02/db-create.sh
+++ c03/p02/db-create.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uvt-kvm create db release=xenial arch=amd64 --user-data=db.cfg --cpu 2 --disk=5 --memory=512 --bridge=virbr0 
+uvt-kvm create db release=xenial arch=amd64 --user-data=db.yaml --cpu 2 --disk=5 --memory=512 --bridge=virbr0
--- c03/p02/web-create.sh
+++ c03/p02/web-create.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uvt-kvm create web release=xenial arch=amd64 --user-data=web.cfg --cpu 2 --disk=5 --memory=512 --bridge=virbr0 
+uvt-kvm create web release=xenial arch=amd64 --user-data=web.yaml --cpu 2 --disk=5 --memory=512 --bridge=virbr0
