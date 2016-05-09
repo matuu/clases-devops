03cbee1afc7c57199e7a0e5fa30ed3e6aaef13aa
JuanJo Ciarlante <juanjosec@gmail.com>
2016-05-09 19:14:37 -0300

    [jjo] herramientas-uvtool.sh: cosmetics

--- c03/p01/herramientas-uvtool.sh
+++ c03/p01/herramientas-uvtool.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
-export http_proxy=http://172.16.16.1:8000/
+echo "# Optimizacion: proxy local"
+echo "export http_proxy=http://172.16.16.1:8000/"
+echo ""
 
-echo "Instalación"
+echo "# Instalación"
 echo "sudo apt-get install uvtool #Desloguearse/Loguear para que tome GID"
 echo ""
 
-echo "Obteniendo imágenes"
+echo "# Obteniendo imágenes"
 echo "uvt-simplestreams-libvirt -v sync release=xenial arch=amd64 --source http://cloud-images.ubuntu.com/releases"
 echo ""
 
-echo "keys"
+echo "# SSH keys (locales, podés copiar las tuyas en vez)"
 echo "ssh-keygen"
 echo ""
 
-echo "Lanzando una instancia"
+echo "# Lanzando una instancia"
 echo "uvt-create primervm"
 echo ""
 
-echo "Ver IP"
+echo "# Ver IP"
 echo "uvt-kvm ip primervm"
 echo ""
 
 
-echo "Loguearse"
+echo "# Loguearse"
 echo "uvt-kvm ssh primervm --insecure"
 echo ""
 
-echo "Eliminar"
+echo "# Eliminar"
 echo "uvt-kvm destroy primervm"
 echo ""
 
-echo "Creando con parámetros"
+echo "# Creando con parámetros"
 echo "uvt-kvm create segundavm release=xenial arch=amd64 --bridge virbr0 --memory 512 --disk 10 --memory 2048"
 echo ""
-
