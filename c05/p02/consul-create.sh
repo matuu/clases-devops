3c5291b33742f01f635595220339f2c9934ac69f
Diego Navarro <navarrow@gmail.com>
2016-05-24 02:42:36 -0300

    [navarrow] class 05/p02 - initial commit

---
+++ c05/p02/consul-create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+name=consul
+cloud_init_file="consul.yaml"
+image=$(nova image-list | awk '/xenial.*disk1.img/{ print $4 }')
+flavor=m1.small
+net_id=$(nova net-list | awk '/net_umstack/{ print $2 }')
+set -x
+nova boot --image ${image} --nic net-id="${net_id}" --flavor ${flavor} --user-data ${cloud_init_file} "$@" ${name}
+
+neutron floatingip-create --floating-ip-address 192.168.3.130 ext_net
+nova floating-ip-associate consul 192.168.3.13
---
+++ c05/p02/db-create.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+name=c05-${USER}-db
+cloud_init_file="db.yaml"
+image=$(nova image-list | awk '/xenial.*disk1.img/{ print $4 }')
+flavor=m1.small
+net_id=$(nova net-list | awk '/net_umstack/{ print $2 }')
+set -x
+nova boot --image ${image} --nic net-id="${net_id}" --flavor ${flavor} --user-data ${cloud_init_file} "$@" ${name}
