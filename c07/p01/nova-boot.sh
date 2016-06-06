b990a877309e2ea117359a6fb9717ca93dbc734b
JuanJo Ciarlante <juanjosec@gmail.com>
2016-06-06 02:10:35 +0000

    [jjo] c07/p01: require ENV={prod,stag}, add make snap-create; rename hostnames as $ENV-$USER-{db,web-N,fe}

--- c07/p01/nova-boot.sh
+++ c07/p01/nova-boot.sh
@@ -2,7 +2,7 @@
 name=${1:?missing name}
 cloud_init_file=${2:?missing yaml file}
 shift 2
-grep -q EDITAR_USUARIO ${cloud_init_file} && { echo "ERROR: tenes que editar EDITAR_USUARIO con tu nombre en ${cloud_init_file}" ; exit 1; }
+grep -q EDITAR_ ${cloud_init_file} && { echo "ERROR: tenes que editar EDITAR_... en ${cloud_init_file}" ; exit 1; }
 image=$(nova image-list | awk '/xenial.*disk1.img/{ print $4 }')
 flavor=m1.small
 net_id=$(nova net-list | awk '/net_umstack/{ print $2 }')
