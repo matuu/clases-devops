449500fd23dd6f5144dcf8e7c59049f75cedae90
pfgomezv <pgomez@sapito.novalocal>
2016-07-29 22:12:11 +0000

    user.terraformrc

--- c12/p01/00-init-terraform.sh
+++ c12/p01/00-init-terraform.sh
@@ -2,9 +2,9 @@
 
 # seteamos la variables para terraform
 
-PATH=/usr/local/terraform/bin:/home/$USER/terraform:$PATH
-export TF_VAR_USER=$USER;
-export TF_VAR_PASS=$OS_PASSWORD;
+#export PATH=/usr/local/terraform/bin:/home/$USER/terraform:$PATH
+#export TF_VAR_USER=$USER;
+#export TF_VAR_PASS=$OS_PASSWORD;
 
 cd conf/tmpconsul/
 wget http://192.168.3.251/consul/0.6.4/consul_0.6.4_linux_amd64.zip
