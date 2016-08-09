11a83eccd5120a80d29b15d4160d5c1a0afa41d6
navarrow <diego.navarro@um.edu.ar>
2016-08-09 23:12:12 +0000

    [navarrow] fix yaml multi-user :)

--- c13/p01/30-kube-run.sh
+++ c13/p01/30-kube-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
-gcloud compute disks create --size=200GB  mysql-disk
+gcloud compute disks create --size=200GB  $USER-mysql-disk
+sed -i "s/ mysql-disk/ $USER-mysql-disk/g" db-deployment.yaml
 kubectl create -f .
--- c13/p01/99-kube-delete.sh
+++ c13/p01/99-kube-delete.sh
@@ -2,4 +2,4 @@
 set -x
 kubectl delete -f .
 sleep 5
-gcloud compute disks delete mysql-disk
+gcloud compute disks delete $USER-mysql-disk
